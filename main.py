255f661802831da50c9d7c52acbc5cbc1c033d91
enricoenne <enrico.negri97@gmail.com>
2025-02-25 23:59:04 +0100

    generator functions

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from itertools import chain
 
-
-
-
 def plot_scaled_letter(unicode_num=65, fontname="Arial", target_size=20, dpi=20, show = False):
     """Plots a letter and scales its bounding box to exactly match target_size×target_size pixels."""
     print(f"output/{fontname}_{unicode_num}")
@@ -49,17 +46,21 @@ def plot_scaled_letter(unicode_num=65, fontname="Arial", target_size=20, dpi=20,
     plt.close(fig)  # Explicitly close the figure to prevent memory leaks
 
 
+def generator(font_list):
+    # A = 65
+    # a = 97
+
+    letters_range = list(chain(range(65, 65+26), range(97, 97+26)))
+
+    for f in font_list:
+        for n in letters_range:
+            plot_scaled_letter(unicode_num=n, fontname=f)
+
+
 fonts = ["Arial", "Times New Roman", "Courier New", "Comic Sans MS", "DejaVu Sans"]
 
 plot_scaled_letter(fontname=fonts[2], unicode_num=107, show=True)
 
-# A = 65
-# a = 97
-
-letters_range = list(chain(range(65, 65+26), range(97, 97+26)))
-
-for f in fonts:
-    for n in letters_range:
-        plot_scaled_letter(unicode_num=n, fontname=f)
+generator(fonts)
 
 
